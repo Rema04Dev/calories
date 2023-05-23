3d38175b9d19ee22aab263745fa40cc53f2af6f9
Roman <romansmerdyakov@gmail.com>
2023-05-23 19:11:41 +0600

    add unit test

---
+++ src/utils/index.test.ts
@@ -0,0 +1,61 @@
+import { describe, it, expect } from 'vitest';
+import calculateCalories from './calculateCalories';
+import { ICalories, IFormValues } from '../types';
+
+interface ITestCase {
+  received: IFormValues;
+  expected: ICalories;
+}
+
+const testCases: Array<ITestCase> = [
+  {
+    received: {
+      gender: 'male',
+      age: '30',
+      height: '180',
+      weight: '85',
+      activity: 'min',
+    },
+    expected: {
+      weightMaintenance: 2196,
+      loseWeight: 1866,
+      gainWeight: 2525,
+    },
+  },
+
+  {
+    received: {
+      gender: 'female',
+      age: '25',
+      height: '168',
+      weight: '64',
+      activity: 'low',
+    },
+    expected: {
+      weightMaintenance: 1930,
+      loseWeight: 1640,
+      gainWeight: 2220,
+    },
+  },
+
+  {
+    received: {
+      gender: 'male',
+      age: '40',
+      height: '175',
+      weight: '95',
+      activity: 'high',
+    },
+    expected: {
+      weightMaintenance: 3189,
+      loseWeight: 2710,
+      gainWeight: 3667,
+    },
+  },
+];
+
+describe('calculate calories', () => {
+  test.each(testCases)('calories', ({ received, expected }) => {
+    expect(calculateCalories(received)).toEqual(expected);
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './tests/setup.js',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
   },
+  plugins: [react()],
 });
